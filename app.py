663870679246ca5e16476b55b3de411dc942bb0c
KIYOTAKA401 <129326773+KIYOTAKA401@users.noreply.github.com>
2025-05-29 00:43:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 
 # Reemplaza con tu URL y API KEY
 SUPABASE_URL = "https://azwanfinaeztivngsnlv.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."  # Truncado por seguridad
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6d2FuZmluYWV6dGl2bmdzbmx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0OTE0MDEsImV4cCI6MjA2NDA2NzQwMX0.w9QmRZh_dro2xd9J85NYHamgzOkaKGXDN01SwCbdkEI"  # Truncado por seguridad
 HEADERS = {
     "apikey": SUPABASE_KEY,
     "Authorization": f"Bearer {SUPABASE_KEY}",
